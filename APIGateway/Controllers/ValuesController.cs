e97519d7ea75416cb73acf36248be7f2459ea221
Asif Rahman <asif0531@live.com>
2019-10-18 23:48:15 +0600

    API gateway added

---
+++ APIGateway/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "API GATEWAY";
+        }
+    }
+}
